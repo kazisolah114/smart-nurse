b354e192f920eb6358863ea72acee8e0830a9ea5
Kazi Solayman <kazisolayman123@gmail.com>
2025-04-20 02:46:32 +0600

    Added sidebar and dashboard header UI with navigation

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import Header from '@/dashboard/Header/Header';
+import Sidebar from '@/dashboard/Sidebar/Sidebar';
 
 const DashboardLayout = ({ children }) => {
     return (
-        <main>
-            <header>Dashboard header</header>
-            <div className='grid grid-cols-12'>
-                <aside className='col-span-2'>
-                    Sidebar
-                </aside>
-                <div className='col-span-10'>
+        <main className='grid grid-cols-12 h-screen'>
+            <Sidebar />
+            <div className='col-span-10'>
+                <Header />
+                <div className='p-3'>
                     {children}
                 </div>
             </div>
---
+++ src/dashboard/Header/Header.jsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useState } from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import { Bell } from 'lucide-react';
+
+const Header = () => {
+    const [notifications, setNotifications] = useState(3)
+    return (
+        <header className='flex px-3 py-4 border-b'>
+            <div className="ml-auto flex items-center gap-4">
+                <Button size="icon" variant="ghost" className="relative cursor-pointer" onClick={() => setNotifications(0)}>
+                    <Bell className="h-5 w-5" />
+                    {notifications > 0 && (
+                        <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                            {notifications}
+                        </span>
+                    )}
+                </Button>
+                <div className="flex items-center gap-3">
+                    <Avatar>
+                        <AvatarImage src="/placeholder.svg?height=40&width=40" alt="User" />
+                        <AvatarFallback size="1px"></AvatarFallback>
+                    </Avatar>
+                    <div className="hidden lg:block">
+                        <p className="text-sm font-medium">Sarah Nurse</p>
+                        <p className="text-xs text-gray-500">RN, Emergency Department</p>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
